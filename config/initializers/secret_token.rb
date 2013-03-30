181f0a105067b718c0ea107ae55b82c875e18c55
colorrin <mb.andres@gmail.com>
2013-03-30 18:22:19 -0300

    Initial Rails Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cafependiente::Application.config.secret_token = '83ab30572b03aa25ea3605e27f71049bba632f02dc81cb4bca4c50361b4c1de88fda30f707a5488ee08ab61055882f6d345ce5872d64071047a2683f859451ee'
