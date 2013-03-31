a2599c3155d953884739f808abc53a1b78fa4066
colorrin <mb.andres@gmail.com>
2013-03-31 00:16:02 -0300

    Added basic shop creation

--- app/controllers/cafeterias_controller.rb
+++ app/controllers/cafeterias_controller.rb
@@ -3,5 +3,16 @@ class CafeteriasController < ApplicationController
   end
 
   def new
+  	 @shop = Shop.new(params[:survey])
+
+    respond_to do |format|
+      if @shop.save
+        format.html
+        format.json { render json: @shop, status: :created, location: @shop }
+      else
+        format.html
+        format.json { render json: @shop.errors, status: :unprocessable_entity }
+      end
+  	end
   end
 end
